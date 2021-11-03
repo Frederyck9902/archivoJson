2867b1c92f6d036c1961028ad320779b9f9c535a
Combatmars123 <cris190199@gmail.com>
2021-11-03 13:38:38 -0500

    se realizo paginacion,filtro y busqueda utilizando datatables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto009';
-
+export class AppComponent implements OnDestroy,OnInit {
+  title = 'Hurtos en Colombia';
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
   articulos:any = null;
 
   constructor(private http: HttpClient){}
 
   ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 20,
+      language: {
+        url:'//cdn.datatables.net/plug-ins/1.11.3/i18n/es-mx.json'
+      }
+    };
     this.http.get("https://www.datos.gov.co/resource/6sqw-8cg5.json")
       .subscribe(
-        result => {
+        (result) => {
           this.articulos = result;
+          this.dtTrigger.next();
         },
         error => {
           console.log('Problemas');
         }
       );
+  }
+
+  ngOnDestroy(){
+    this.dtTrigger.unsubscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { DataTablesModule } from "angular-datatables";
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -9,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
